af77508ddc4dca70b45e5c93125ccbdeaa701033
zhoujunyu <1009967133@qq.com>
2023-03-16 22:05:25 +0800

    ZHOU-05 存在的意义是什么

---
+++ src/main/java/com/zhou/ZStreamBuilder.java
@@ -0,0 +1,10 @@
+package com.zhou;
+
+/**
+ * 我应该给用户一个很好的环境,进行一个快速的入门
+ * 使用一个结构化的方法给客户
+ * @author 周俊宇
+ */
+public class ZStreamBuilder {
+
+}
---
+++ src/main/java/com/zhou/in/InStream.java
@@ -0,0 +1,8 @@
+package com.zhou.in;
+
+/**
+ * 他是一个适配器吧,用于适配各种消息组件
+ * @author 周俊宇
+ */
+public class InStream {
+}
